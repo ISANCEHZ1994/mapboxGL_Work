4f7f5309e6e7ca00a9df978b2d49fae2a0c1a225
Israel Sanchez <Israel.pro1994@gmail.com>
2021-05-24 14:03:23 -0500

    created state as well as imported mapbox css

--- mappingdata/src/Map.js
+++ mappingdata/src/Map.js
@@ -2,3 +2,33 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import mapboxgl from 'mapbox-gl';
 
+export default class Map extends React.Component{
+    
+    state = {
+        lng: 0,
+        lat: 0,
+        zoom: 0
+    }
+
+    componentDidMount(){
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng,this.state.lat],
+            zoom: this.state.zoom
+        })
+    }
+
+    render(){
+
+
+        return(
+
+            <div>
+
+            </div>
+
+        );
+    };
+
+};
