2ee29235dc79a9b2a335409f53bba4416cd7fe88
Israel Sanchez <Israel.pro1994@gmail.com>
2021-05-24 17:13:17 -0500

    now map starts in Houston, TX

--- mappingdata/src/Map.js
+++ mappingdata/src/Map.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// import ReactMapGL from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 // import 'mapbox-gl/dist/mapbox-gl.css';
 
+// mapboxgl
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXM5NHJlYWwxMyIsImEiOiJja3AyemR5NnMwMDNxMnZwYzRqOWZ3YjBtIn0.3_R4gBCcmuHZdEfWYt3LCw';
+
+// sample data of the subway stations in new york
+// array of data
+// items are objects
+// in each object: location, city, state, and coordinates all string values EXCEPT coordinates
+
 
 export default class Map extends React.Component{
     
     state = {
-        lng: -29.7604,
-        lat: 95.3698,
-        zoom: 10,
-        width: window.innerWidth,
-        height: window.innerHeight
+        lng: -95.369804,
+        lat: 29.760427,
+        zoom: 9,
+       
     };
 
     componentDidMount(){
 
-        const map = new mapboxgl.Map({
+        const map = new mapboxgl.Map({ //new mapboxgl.Map({
             container: this.mapContainer, // conatiner ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [this.state.lng,this.state.lat], // Starting Position
+            center: [ this.state.lng , this.state.lat ], // Starting Position
             zoom: this.state.zoom 
         })
+
+        // map.fitBounds([ -29.7604, 95.3698 ])
+
+        let marker = new mapboxgl.Marker() // mapboxgl
+        // .setLngLat([-36.2048,138.2529])
 
     };
 
@@ -32,9 +44,8 @@ export default class Map extends React.Component{
         return(
 
             <div>
-                <div ref={ el => this.mapContainer = el } style={{width: '5000px', height: '250vh'}}>
-
-                </div>
+                <div ref={ el => this.mapContainer = el } style={{width: '100%', height: '100vh'}}> </div>
+                {/* <ReactMapGL {...this.state} /> */}
 
             </div>
 
