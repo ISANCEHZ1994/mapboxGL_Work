4d73f6c7082d71f7a3e8fa0c31282a856082a013
Israel Sanchez <Israel.pro1994@gmail.com>
2021-05-24 15:50:28 -0500

    Houston, TX will not appear - says invaild must be between 90 and -90

--- mappingdata/src/Map.js
+++ mappingdata/src/Map.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import mapboxgl from 'mapbox-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXM5NHJlYWwxMyIsImEiOiJja3AyemR5NnMwMDNxMnZwYzRqOWZ3YjBtIn0.3_R4gBCcmuHZdEfWYt3LCw';
 
 export default class Map extends React.Component{
     
     state = {
-        lng: 0,
-        lat: 0,
-        zoom: 0
-    }
+        lng: -29.7604,
+        lat: 95.3698,
+        zoom: 10,
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
 
     componentDidMount(){
+
         const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [this.state.lng,this.state.lat],
-            zoom: this.state.zoom
+            container: this.mapContainer, // conatiner ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [this.state.lng,this.state.lat], // Starting Position
+            zoom: this.state.zoom 
         })
-    }
+
+    };
 
     render(){
 
@@ -25,6 +32,9 @@ export default class Map extends React.Component{
         return(
 
             <div>
+                <div ref={ el => this.mapContainer = el } style={{width: '5000px', height: '250vh'}}>
+
+                </div>
 
             </div>
 
